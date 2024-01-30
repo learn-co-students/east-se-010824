0ec14f7fc57c74b476324b255e64ea182faccc4a
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-01-30 09:30:23 -0800

    DONE: part one state and events

--- 09-state-and-events/project_showcase/src/components/Header.js
+++ 09-state-and-events/project_showcase/src/components/Header.js
@@ -1,11 +1,18 @@
+import { useState } from "react"
+
 function Header() {
+    const [ darkMode, setDarkMode ] = useState(true)
+    
+    const handleModeClick = () => setDarkMode(!darkMode)
+    const buttonText = darkMode ? "Light Mode" : "Dark Mode"
+
     return (
         <header>
             <h1>
                 <span className="logo">{"//"}</span>
                 Project Showcase
             </h1>
-            <button>Dark Mode</button>
+            <button onClick={handleModeClick}>{ buttonText }</button>
         </header>
     )
 }
--- 09-state-and-events/project_showcase/src/components/ProjectListItem.js
+++ 09-state-and-events/project_showcase/src/components/ProjectListItem.js
@@ -1,3 +1,23 @@
+// 5. Define a `ProjectListItem` component that:
+
+// - Accepts the props argument
+
+// - Destructure the props object and return `project`
+
+// - Destructure the properties of the `project` object
+
+// - Create an `img` element nested inside a `figure` element
+
+// - Set the `img` property `src` to the `img` variable created when destructuring
+
+// - Set the `img` property `alt` to `project: ${name}`
+
+// - Create an `article` element with `h4` and `p` tags nested inside
+
+//   - Dynamically render each project name inside the `h4` tag
+
+//   - Dynamically render each project's about data inside the `p` tag
+
 function ProjectListItem() {
     return (
         <article>
