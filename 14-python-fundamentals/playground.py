d1ea83d88fc04846a931d8a7e45b8a9a372f5986
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-02-21 08:42:34 -0800

    DONE: python fundamentals

---
+++ 14-python-fundamentals/playground.py
@@ -0,0 +1,58 @@
+variable_1 = "hello World" 
+# print(variable_1)
+# variable_1 = "a different string"
+# print(variable_1)
+# strings
+# print(f'greeting: {variable_1}')
+# javaScript equivalent: `greeting: ${variable1}`
+# integer = 1
+# lists = ["hello", "world", 1, 3]
+# print(dir(lists))
+# lists.remove(lists[2])
+# lists.insert(1, "hello")
+# print(lists.count('hello'))
+# tuples = (1,2,3)
+# print(tuples)
+# our_set = set([1,1,2,3,3,4,5,5,7])
+# print(our_set)
+# our_set.remove(2)
+# print(list(our_set))
+
+# dictionary = {
+#     "key": "value",
+#     1: 100 
+# }
+
+# print(dictionary['key'])
+
+# if "condition num 1":
+#     print('1st condition true')
+# elif not "condition num 2":
+#     print('2nd condition true')
+# else:
+#     print('both conditions false')
+
+
+# variable = "return if true" if not False else "return if false"
+# false ? "if true" : "if false"
+# print(variable)
+
+num_to_reach = 10
+current_num = 1
+
+# while current_num < num_to_reach:
+#     print(current_num)
+#     current_num += 1
+list_of_strings = ["hello", "world", 'hi']
+# for word in list_of_strings:
+    # print(word)
+
+
+new_list = [word * 3 for word in list_of_strings if len(word) > 3]
+# print(new_list)
+# def hello():
+#     pass
+# print("hello" in "hello world")
+
+# print(list_of_strings[0:3])
+max([1,2,10])
