33407cdb02e139401606d8c9ec79699f60aaa887
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-02-23 07:46:13 -0800

    ADD: starter code many-to-many lecture

---
+++ 17-OO-many-to-many/lib/museum.py
@@ -0,0 +1,4 @@
+# Create Museum class
+    # create relationship to visit
+
+    # create relationship to visitor
---
+++ 17-OO-many-to-many/lib/visitor.py
@@ -0,0 +1,4 @@
+# create Visitor class
+    # create relationship to visit
+
+    # create relationship to museum
